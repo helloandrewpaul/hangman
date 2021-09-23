36f9fba5e7072e87ef2ee4fe06879998616264ef
“Andrew <andrewpaulstudio@gmail.com>
2021-09-23 02:48:07 -0400

    notifications added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
+import Notification from './components/Notification';
+import Popup from './components/Popup';
+import { showNotification as show } from './helpers/helpers';
 
 import './App.css';
 
@@ -13,14 +16,42 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
   return (
     <>
       <Header />
       <div className='game-container'>
-        <Figure />
+        <Figure wrongLetters={wrongLetters} />
         <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
+      <Popup />
+      <Notification showNotification={showNotification} />
     </>
   );
 }
