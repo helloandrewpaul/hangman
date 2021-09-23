cd85bf96fc342d6a1a76dd2cfea87b32e7c13ac8
“Andrew <andrewpaulstudio@gmail.com>
2021-09-23 19:17:42 -0400

    Added styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-    return (
-        <>
-            <h1>Hangman</h1>
-        <p>Find the hidden word - Enter a letter</p></>
-    )
-}
+  return (
+    <Wrap>
+      <Head>Hangman</Head>
+      <Tag>Find the hidden word - Enter a letter</Tag>
+    </Wrap>
+  );
+};
 
-export default Header
+export default Header;
+
+const Wrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Head = styled.h1`
+  display: inline-flex;
+  justify-content: center;
+`;
+const Tag = styled.p`
+  display: inline-flex;
+  justify-content: center;
+`;
