8a1d7749acfe07d5f040d2a833309a5218209232
“Andrew <andrewpaulstudio@gmail.com>
2021-09-23 00:27:45 -0400

    correct letter component added

---
+++ src/components/WrongLetters.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WrongLetters = () => {
+    return (
+        <div class='wrong-letter-container'>
+            <div id='wrong-letters'></div>
+            
+        </div>
+    )
+}
+
+export default WrongLetters
