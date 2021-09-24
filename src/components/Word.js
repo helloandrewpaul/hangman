abce2769159f974fd83527abf5681c6b1d14d2a5
“Andrew <andrewpaulstudio@gmail.com>
2021-09-23 21:37:42 -0400

    Word added

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import styled from 'styled-components'
 
 const Word = ({ selectedWord, correctLetters }) => {
   return (
-    <div className='word'>
+    <WordDiv >
       {selectedWord.split('').map((letter, index) => {
         return (
-          <span className='letter' key={index}>
+          <Letter key={index}>
             {correctLetters.includes(letter) ? letter : ''}
-          </span>
+          </Letter>
         );
       })}
-    </div>
+    </WordDiv>
   );
 };
 
 export default Word;
+
+const Letter = styled.span`
+  border-bottom: 3px solid #000;
+  display: inline-flex;
+  font-size: 30px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 3px;
+  height: 50px;
+  width: 20px;`
+
+const WordDiv = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);`
+
+
--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-const WrongLetters = ({wrongLetters}) => {
-    return (
-        <div className='wrong-letter-container'>
-            <div>
-                {wrongLetters.length > 0 && <p>Graveyard:</p>}
-                {wrongLetters.map((letter, index) => <span key={index}>{letter}</span>).reduce((previous, current)=> previous === null?[current]:[previous, ', ' ,current], null)}
-                
-            </div>
-            
-        </div>
-    )
-}
+const WrongLetters = ({ wrongLetters }) => {
+  return (
+    <GraveYardWrap>
+      <div>
+        {wrongLetters.length > 0 && <GraveYard>Graveyard:</GraveYard>}
+        {wrongLetters
+          .map((letter, index) => <Dead key={index}>{letter}</Dead>)
+          .reduce(
+            (previous, current) =>
+              previous === null ? [current] : [previous, ', ', current],
+            null
+          )}
+      </div>
+    </GraveYardWrap>
+  );
+};
 
-export default WrongLetters
+export default WrongLetters;
+
+const Dead = styled.div`
+  font-size: 24px;
+`;
+
+const GraveYard = styled.p`
+  margin: 0 0 5px;
+`;
+
+const GraveYardWrap = styled.div`
+width:100%;
+  position: absolute;
+bottom:0;
+  display: flex;
+  flex-direction: column;
+  text-align: right;
+`;
+
+
